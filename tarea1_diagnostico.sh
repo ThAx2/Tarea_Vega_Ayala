2101a1f78e32f390c1306a6a729f73772d7ebded
ThAx2 <MundoRamdoMx@gmail.com>
2026-02-03 06:10:20 +0000

    Scripts de diagnóstico/CheckStatus para Windows y Linux

---
+++ tarea1_diagnostico.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "--------------------------------------" 
+echo -e "\nEspacio en disco disponible \n" 
+df -hT
+echo ""
+echo -e "--------------------------------------\n" 
+echo  -n "Nombre de la maquina - "; hostname 
+echo -n "IP: "; hostname -I
+
